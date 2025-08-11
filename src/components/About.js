9fefcae909a84be7de4324aa3985ddcfc9c2fc62
yull03 <rlaaudtjr67@gmail.com>
2025-08-12 00:02:43 +0900

    crt

--- src/components/About.js
+++ src/components/About.js
@@ -1,52 +1,49 @@
-import { GiSecretBook } from "react-icons/gi";
-import { GiTalk } from "react-icons/gi";
+import { GiSecretBook, GiTalk } from "react-icons/gi";
 import { PiMedalBold } from "react-icons/pi";
 import { SlEnergy } from "react-icons/sl";
 
 const About = () => {
-    return (
-        <div>
-            <h1>About Me</h1>
-            <div>
-                <div className="back-img">
-                    <div className="text-list">
-                    <ul>
-                        <p><GiSecretBook /></p>
-                        <li>배움을 통한 자기 성장의 즐거움을 만끽하자! </li>
-                    </ul>
-                    <ul>
-                        <p><GiTalk /></p>
-                        <li>커뮤니케이션을 통해 긍정적 영향을 극대화 </li>
-                    </ul>
-                    <ul>
-                        <p><PiMedalBold /></p>
-                        <li>성취감을 위한 도전은 두렵지 않다! </li>
-                    </ul>
-                    <ul>
-                        <p><SlEnergy /></p>
-                        <li> 끈기있게 끝까지 절대포기 하지 않아! </li>
-                    </ul>
-                    </div>
-                    <div className="skill">
-                        <h3>My-skill</h3>
-                        <div className="skill-img">
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <div className="center-sass"><img className="sass"/></div>
-                        </div>
-                    </div>
-                </div>
+  const items = [
+    { icon: <GiSecretBook />, text: "배움을 통한 자기 성장의 즐거움을 만끽하자!" },
+    { icon: <GiTalk />,       text: "커뮤니케이션을 통해 긍정적 영향을 극대화" },
+    { icon: <PiMedalBold />,  text: "성취감을 위한 도전은 두렵지 않다!" },
+    { icon: <SlEnergy />,     text: "끈기있게 끝까지 절대 포기하지 않아!" },
+  ];
+
+  return (
+    <div>
+      <h1>About Me</h1>
+      <div>
+        <div className="back-img">
+          <div className="text-list">
+            {items.map((it, i) => (
+              <ul key={i} className="about-row">
+                <p className="about-icon">{it.icon}</p>
+                <li>{it.text}</li>
+              </ul>
+            ))}
+          </div>
+
+          <div className="skill">
+            <h3>My-skill</h3>
+            <div className="skill-img">
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <img/>
+              <div className="center-sass"><img className="sass" /></div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+
 import About from "./About";
 import Clone from "./Clone";
 import Footer from "./Footer";
@@ -7,52 +8,50 @@ import Project from "./Project";
 import Publishing from "./Publishing";
 
 const Main = () => {
+
     return (
         <div id="all-style">
-            <div className="top">
-            <div className="image">
-                <img/>
-            </div>
-            <div className="main-text">
-                <p className="one">Front</p>
-                <p className="two">End</p>
-                <p className="three">PORTFOLIO</p>
-            </div>
-            </div>
-            <Header/>
-            <div className="main-bottom">
-                <div className="img-box">
-                    <img/>
-                    <div className="box-text">
-                    <ul>
-                        <h2>Phone</h2>
-                        <li>010-4721-6271</li>
-                    </ul>
-                    <ul>
-                        <h2>E-mail</h2>
-                        <li>yull03@naver.com</li>
-                    </ul>
-                    </div>
-                </div>
-                <div className="bottom-text">
-                    <ul className="my-text">
-                        <h1>Kim-Yull</h1>
-                        <li>협업과 성장을 즐기는 프론트앤드 개발자</li>
-                    </ul>
-                    <ul className="git">
-                        <h3>Git-hub :</h3>
-                        <li><a href="https://github.com/yull03">https://github.com/yull03</a></li>
-                    </ul>
-
-                </div>
-            </div>
-            <About/>
-            <Project/>
-            <List/>
-            <Clone/>
-            <Publishing/>
-            <Footer/>
+        <div className="top">
+          <div className="image"><img alt="" /></div>
+          <div className="main-text">
+            <p className="one">Front</p>
+            <p className="two">End</p>
+            <p className="three">PORTFOLIO</p>
+          </div>
         </div>
+  
+        {/* Header 래퍼에 ref만 둠 */}
+        <div className="sticky-header">
+        <Header />
+      </div>
+  
+        <div className="main-bottom">
+          <div className="img-box">
+            <img alt="" />
+            <div className="box-text">
+              <ul><h2>Phone</h2><li>010-4721-6271</li></ul>
+              <ul><h2>E-mail</h2><li>yull03@naver.com</li></ul>
+            </div>
+          </div>
+          <div className="bottom-text">
+            <ul className="my-text">
+              <h1>Kim-Yull</h1>
+              <li className="sub-text">협업과 성장을 즐기는 프론트앤드 개발자</li>
+              <div className="git">
+                <h3>Git-hub :</h3>
+                <li><a href="https://github.com/yull03">https://github.com/yull03</a></li>
+              </div>
+            </ul>
+          </div>
+        </div>
+  
+        <About />
+        <Project />
+        <List />
+        <Clone />
+        <Publishing />
+        <Footer />
+      </div>
     );
 };
 
