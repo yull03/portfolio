c00982b81c6d0927588c8e30d067867bee043d70
yull03 <rlaaudtjr67@gmail.com>
2025-08-12 14:25:45 +0900

    create

--- src/components/Publishing.js
+++ src/components/Publishing.js
@@ -8,19 +8,19 @@ const Publishing = () => {
             <div id="card-wrap">
                 <div className="item-title"><h2>BLOG</h2></div>
                 <div className="item-card">
-                    <img />
+                    <a href="https://yull03.github.io/logger/index.html" target="_blank"><img src={`${process.env.PUBLIC_URL}/images/blog.png`}/></a>
                 </div>
                 <div className="item-title"><h2>Pinterest</h2></div>
                 <div className="item-card">
-                    <img />
+                    <a href="https://yull03.github.io/pinterest/images.html" target="_blank"><img src={`${process.env.PUBLIC_URL}/images/pinterest.png`}/></a>
                 </div>
                 <div className="item-title"><h2>Yes&#8209;24</h2></div>
                 <div className="item-card">
-                    <img />
+                    <a href="https://yull03.github.io/bookstore/index-2.html" target="_blank"><img src={`${process.env.PUBLIC_URL}/images/yes24.png`}/></a>
                 </div>
                 <div className="item-title"><h2>Trabel</h2></div>
                 <div className="item-card">
-                    <img />
+                    <a href="https://yull03.github.io/0530/index.html" target="_blank"><img src={`${process.env.PUBLIC_URL}/images/trable.png`}/></a>
                 </div>
             </div>
         </section>
